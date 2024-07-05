9b4539cd2480e29559e93747d4dd42629d4d7e94
MarkusKhoa <dangkhoa20006@gmail.com>
2024-07-05 10:21:19 +0700

    Add saving feedback function

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
+import os
+
 from setfit import SetFitModel
 
 @st.cache_resource
@@ -16,6 +19,13 @@ def evaluate_model(predictions, test_labels):
     accuracy = (predictions == test_labels).mean()
     return accuracy
 
+def save_feedback(data, file_path="demo_feedback.csv"):
+    df = pd.DataFrame([data])  # Create a DataFrame with the new data
+    if os.path.exists(file_path):
+        df.to_csv(file_path, mode='a', header=False, index=False)  # Append to the existing file
+    else:
+        df.to_csv(file_path, index=False)  
+
 def main():
     # First Streamlit command: set_page_config
     st.set_page_config(page_title='Testing Sensitive Sentences', page_icon=':books:')
@@ -24,7 +34,6 @@ def main():
     loaded_model = load_model(saved_checkpoint)
     
     st.title("Testing Sensitive sentences")
-    
     res_dict = {0: "normal", 1: "sensitive"}
     
     # Allow user question
@@ -32,6 +41,16 @@ def main():
     if user_question:
         prediction = predict(loaded_model=loaded_model, test_sents=user_question)
         st.write("Prediction: ", res_dict[prediction])
+        
+        feedback_label = st.radio("Is this prediction correct?", ("Yes", "No"))
+        if st.button("Submit Feedback"):
+            feedback_data = {
+                "text": user_question,
+                "prediction": res_dict[prediction],
+                "feedback": feedback_label
+            }
+            save_feedback(data = feedback_data, file_path = "official_feedback_data.csv")
+            st.success("Feedback submitted")
     
 if __name__ == '__main__':
     main()
