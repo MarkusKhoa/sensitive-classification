33ac047825754d13c186e41c7e426639807b93c9
MarkusKhoa <dangkhoa20006@gmail.com>
2024-07-07 18:33:50 +0700

    update connection string

--- database.py
+++ database.py
@@ -9,8 +9,8 @@ load_dotenv()
 def init_mongo_collection():
     mongo_pass = os.getenv("MONGODB_PASSWORD")
     ca = certifi.where()
-    cluster = MongoClient(f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks",
-                          tls = True, tlsCAFile = ca, serverSelectionTimeoutMS =    5000)
+    cluster = MongoClient(f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks&directConnection=True",
+                          tls = True, tlsCAFile = ca, serverSelectionTimeoutMS = 5000)
     db = cluster['ChatbotData']
     collection = db['sensitive_feedbacks']
     return collection
