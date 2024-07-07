28c4f8a0f1782365d28a6b19e6112e0cc9e30a81
MarkusKhoa <dangkhoa20006@gmail.com>
2024-07-07 17:44:34 +0700

    modify ssl certi

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import os
+import certifi
 
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -9,18 +10,9 @@ def init_mongo_collection():
     mongo_pass = os.getenv("MONGODB_PASSWORD")
     cluster = MongoClient(f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks&tlsCAFile=isrgrootx1.pem",
                           tls=True,
-                          tlsAllowInvalidCertificates=True,
-                          serverSelectionTimeoutMS=5000)
+                          tlsAllowInvalidCertificates=True, 
+                          serverSelectionTimeoutMS=5000,
+                          ca = certifi.where())
     db = cluster['ChatbotData']
     collection = db['sensitive_feedbacks']
     return collection
-
-# feedback_data = {
-#                 "text": "How many people are there",
-#                 "prediction": "sensitive",
-#                 "feedback": "Yes"
-#             }
-# collection = init_mongo_collection()
-# collection.insert_one(feedback_data)
-
-# print(f"Insert sample data successfully")   
