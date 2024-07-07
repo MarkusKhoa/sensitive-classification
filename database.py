279639e50a28d41655a0c9fe29be5b9fb3483179
MarkusKhoa <dangkhoa20006@gmail.com>
2024-07-07 17:35:53 +0700

    Add SSL certificates file

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 def init_mongo_collection():
     mongo_pass = os.getenv("MONGODB_PASSWORD")
-    cluster = MongoClient(f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks",
+    cluster = MongoClient(f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks&tlsCAFile=isrgrootx1.pem",
                           tls=True,
                           tlsAllowInvalidCertificates=True,
                           serverSelectionTimeoutMS=5000)
