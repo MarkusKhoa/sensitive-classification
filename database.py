32469d65d49f78db9907018062a16bf5ee465edb
MarkusKhoa <dangkhoa20006@gmail.com>
2024-07-08 09:17:14 +0700

    edit

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ def init_mongo_collection():
     mongo_pass = os.getenv("MONGODB_PASSWORD")
     ca = certifi.where()
     # uri = f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks&directConnection=True"
-    uri = f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks"
+    uri = f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks&directConnection=true"
     cluster = MongoClient(uri, tls = True, tlsCAFile = ca, serverSelectionTimeoutMS = 5000)
     db = cluster['ChatbotData']
     collection = db['sensitive_feedbacks']
