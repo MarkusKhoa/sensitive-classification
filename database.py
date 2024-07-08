4bd7df3287ca3d2a2227817b11e78bcabfab934b
MarkusKhoa <dangkhoa20006@gmail.com>
2024-07-08 09:37:11 +0700

    edit uri

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import os
 import certifi
+import streamlit as st
 
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -9,8 +10,8 @@ load_dotenv()
 def init_mongo_collection():
     mongo_pass = os.getenv("MONGODB_PASSWORD")
     ca = certifi.where()
-    # uri = f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks&directConnection=True"
-    uri = f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks&directConnection=true"
+    # uri = f"mongodb+srv://khoapham:{mongo_pass}@feedbacks.sommds7.mongodb.net/?retryWrites=true&w=majority&appName=feedbacks"
+    uri = st.secrets.db_credentials.DB_URI
     cluster = MongoClient(uri, tls = True, tlsCAFile = ca, serverSelectionTimeoutMS = 5000)
     db = cluster['ChatbotData']
     collection = db['sensitive_feedbacks']
